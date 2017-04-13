5b58368160d9beb046e10b4ddfdcc2ae0f8666ba
Nick Alexander <nick@appkabob.com>
2017-04-12 23:01:24 -0500

    Improved look of report when sorting by different fields

--- connectwise/report.py
+++ connectwise/report.py
@@ -12,6 +12,7 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Image, Spacer, Tabl
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
+
 def hours_to_days(hours):
     if hours == 0:
         return 'Full Day'
@@ -23,7 +24,6 @@ def hours_to_days(hours):
         return 'Full Day'
     elif hours >= 16:
         return str(round(hours / 8, 2)) + ' Days'
-
 
 
 class Report:
@@ -57,8 +57,8 @@ class Report:
             '<font size=8><strong>Date Run:</strong> {}<br />'
             '<strong>Filters:</strong> {}<br />'
             '<strong>Sort:</strong> By {}</font>'.format(
-                date.today().strftime('%Y-%m-%d'),
-                ', '.join(['{}: {}'.format(k, v) for k, v in self.filters.items()]),
+                datetime.today().strftime('%Y-%m-%d at %-I:%M%p'),
+                ' - '.join(['{}: {}'.format(k, v) for k, v in self.filters.items()]),
                 self.sort
             ), styles['Normal'])
 
@@ -66,7 +66,8 @@ class Report:
         header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
 
         # Footer
-        footer = Paragraph('© {} Consortium for Education Change. All rights reserved.'.format(date.today().year), styles['Normal'])
+        footer = Paragraph('© {} Consortium for Education Change. All rights reserved.'.format(date.today().year),
+                           styles['Normal'])
         w, h = footer.wrap(doc.width, doc.bottomMargin)
         footer.drawOn(canvas, doc.leftMargin, h + 16)
 
@@ -82,21 +83,38 @@ class UpcomingSchedule(Report):
         self.name = 'Upcoming Schedule'
         self.output_subdir = 'ConnectWise'
 
+    def save_pdf(self):
+        doc = SimpleDocTemplate(
+            "output/{}/{} By {} Report.pdf".format(self.output_subdir, self.name, self.sort),
+            pagesize=letter,
+            rightMargin=36, leftMargin=36,
+            topMargin=72, bottomMargin=40)
+        Story = self._generate_report_content()
+        doc.build(Story, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
+
     def _generate_report_content(self):
         Story = []
         styles = getSampleStyleSheet()
         styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
 
         logo = "static/img/BeyondComplianceAdminAcademyStorylineLogoHighRes.gif"
-        titletext = "{} Report".format(self.name)
+        titletext = "{} By {} Report<br />{} Team".format(self.name, self.sort, self.filters['Team'])
         # description_text = "This is a schedule report"
 
         im = Image(logo, 2 * inch, 0.84 * inch)
 
+        count_consultants = len(set([schedule_entry.member['identifier'] for schedule_entry in self.schedule_entries]))
+        count_companies = len(set([schedule_entry.name.split(' / ')[0] for schedule_entry in self.schedule_entries]))
+
         # Story.append(im)
         Story.append(Spacer(1, 12))
         Story.append(Paragraph(titletext, styles["Title"]))
-        # Story.append(Spacer(1, 12))
+        Story.append(Spacer(1, 12))
+        Story.append(Paragraph('Schedule Entries: {}'.format(len(self.schedule_entries)), styles["Normal"]))
+        Story.append(Paragraph('Consultants Scheduled: {}'.format(count_consultants), styles["Normal"]))
+        Story.append(Paragraph('Districts: {}'.format(count_companies), styles["Normal"]))
+
+
 
         # schedule_table = [[
         #     Paragraph('<strong>Date</strong>', styles["Normal"]),
@@ -108,7 +126,8 @@ class UpcomingSchedule(Report):
         sorted_schedule_entries = sorted(self.schedule_entries, key=attrgetter('dateStart'))
         if self.sort.lower() == 'consultant':
             # sorted_schedule_entries = sorted(sorted_schedule_entries, key=lambda x: x.member['identifier'][1:])
-            member_identifiers = sorted(set([schedule_entry.member['identifier'] for schedule_entry in sorted_schedule_entries]))
+            member_identifiers = sorted(
+                set([schedule_entry.member['identifier'] for schedule_entry in sorted_schedule_entries]))
             for member_identifier in member_identifiers:
                 schedule_table = []
                 for schedule_entry in sorted(self.schedule_entries, key=attrgetter('dateStart')):
@@ -123,25 +142,28 @@ class UpcomingSchedule(Report):
                                     '%b %-d, %-I:%M%p'),
                                 hours_to_days(schedule_entry.hours)
                             ), styles["Normal"]),
-                            Paragraph(company, styles["Normal"]),
-                            Paragraph('<font size=9>%s</font>' % schedule_name, styles["Normal"])]
+                                Paragraph(company, styles["Normal"]),
+                                Paragraph('<font size=9>%s</font>' % schedule_name, styles["Normal"])]
                         )
                 t = Table(schedule_table, [1.3 * inch, 1.6 * inch, 4.3 * inch])
                 t.setStyle(TableStyle([('VALIGN', (0, 0), (-1, -1), 'TOP'),
-                               ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-                               ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
-                               ]))
+                                       ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                                       ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                                       ]))
+
+                number_of_entries = '{} Schedule Entries'.format(len(schedule_table)) if len(schedule_table) > 1 else '1 Schedule Entry'
 
                 member_section = []
                 member_section.append(Spacer(1, 12))
-                member_section.append(Paragraph('<strong>{}</strong>'.format(member_identifier), styles["Normal"]))
+                member_section.append(Paragraph('<strong>{}</strong> - {}:'.format(member_identifier, number_of_entries), styles["Normal"]))
                 member_section.append(Spacer(1, 4))
                 member_section.append(t)
                 Story.append(KeepTogether(member_section))
 
-        elif self.sort.lower() == 'date':
-            months = sorted(set([utc_to_local(datetime.strptime(schedule_entry.dateStart, '%Y-%m-%dT%H:%M:%SZ')).strftime('%Y-%-m-1')
-                          for schedule_entry in sorted_schedule_entries]))
+        elif self.sort.lower() == 'date' or self.sort.lower() == 'month':
+            months = sorted(set(
+                [utc_to_local(datetime.strptime(schedule_entry.dateStart, '%Y-%m-%dT%H:%M:%SZ')).strftime('%Y-%-m-1')
+                 for schedule_entry in sorted_schedule_entries]))
 
             for month in months:
                 month_table = []
@@ -149,7 +171,8 @@ class UpcomingSchedule(Report):
                     schedule_name = schedule_entry.name.split(' / ')
                     schedule_company_name = schedule_name.pop(0)
                     schedule_name = ' / '.join(schedule_name)
-                    if month == utc_to_local(datetime.strptime(schedule_entry.dateStart, '%Y-%m-%dT%H:%M:%SZ')).strftime('%Y-%-m-1'):
+                    if month == utc_to_local(
+                            datetime.strptime(schedule_entry.dateStart, '%Y-%m-%dT%H:%M:%SZ')).strftime('%Y-%-m-1'):
                         month_table.append(
                             [Paragraph('{}<br />{}'.format(
                                 utc_to_local(
@@ -166,19 +189,23 @@ class UpcomingSchedule(Report):
                                                ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
                                                ]))
                         # Story.append(Paragraph('<strong>{}</strong>'.format(company_name), styles["Normal"]))
-                company_section = []
+
+                number_of_entries = '{} Schedule Entries'.format(len(month_table)) if len(
+                    month_table) > 1 else '1 Schedule Entry'
+
                 Story.append(Spacer(1, 12))
                 print(month)
                 themonth = date(2017, int(month.split('-')[1]), 1).strftime('%B %Y')
                 print(themonth)
-                Story.append(Paragraph('<strong>{}</strong>'.format(themonth), styles["Normal"]))
+                Story.append(Paragraph('<strong>{}</strong> - {}:'.format(themonth, number_of_entries), styles["Normal"]))
                 Story.append(Spacer(1, 4))
                 Story.append(t)
                 # Story.append(company_section)
                 # Story.append(KeepTogether(company_section))
 
         elif self.sort.lower() == 'district' or self.sort.lower() == 'company' or self.sort.lower() == 'organization':
-            company_names = sorted(set([schedule_entry.name.split(' / ')[0] for schedule_entry in sorted_schedule_entries]))
+            company_names = sorted(
+                set([schedule_entry.name.split(' / ')[0] for schedule_entry in sorted_schedule_entries]))
             for company_name in company_names:
                 company_table = []
                 for schedule_entry in sorted(self.schedule_entries, key=attrgetter('dateStart')):
@@ -202,13 +229,17 @@ class UpcomingSchedule(Report):
                                                ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
                                                ]))
                 # Story.append(Paragraph('<strong>{}</strong>'.format(company_name), styles["Normal"]))
-                company_section = []
-                company_section.append(Spacer(1, 12))
-                company_section.append(Paragraph('<strong>{}</strong>'.format(company_name), styles["Normal"]))
-                company_section.append(Spacer(1, 4))
-                company_section.append(t)
-                Story.append(KeepTogether(company_section))
-            # sorted_schedule_entries = sorted(sorted_schedule_entries, key=attrgetter('name'))
+
+                number_of_entries = '{} Schedule Entries'.format(len(company_table)) if len(
+                    company_table) > 1 else '1 Schedule Entry'
+
+                # company_section = []
+                Story.append(Spacer(1, 12))
+                Story.append(Paragraph('<strong>{}</strong> - {}:'.format(company_name, number_of_entries), styles["Normal"]))
+                Story.append(Spacer(1, 4))
+                Story.append(t)
+                # Story.append(KeepTogether(company_section))
+                # sorted_schedule_entries = sorted(sorted_schedule_entries, key=attrgetter('name'))
         else:
             sorted_schedule_entries = sorted(sorted_schedule_entries, key=attrgetter(self.sort))
 
