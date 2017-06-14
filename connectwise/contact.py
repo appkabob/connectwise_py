9755ba833b7515d5737d0c632516a0bc75a82c7f
Nick Alexander <nick@appkabob.com>
2017-06-14 11:42:44 -0500

    Updated Contact class to not require email as a separate init parameter, since we can now get it from get_email() function for the purposes of repr

--- connectwise/contact.py
+++ connectwise/contact.py
@@ -1,33 +1,26 @@
 from .connectwise import Connectwise
-try:
-    import constants
-except ImportError:
-    pass
+import constants
 
 class Contact:
-    def __init__(self, email, **kwargs):
-        self.email = email
+    def __init__(self, **kwargs):
+        # self.id = _id
         for kwarg in kwargs:
             setattr(self, kwarg, kwargs[kwarg])
 
     def __repr__(self):
-        return "<Contact {}>".format(self.email)
+        return "<Contact {} {}>".format(self.id, self.get_email())
 
     @classmethod
     def fetch_by_email(cls, email):
         child_conditions = 'communicationItems/value="{}"'.format(email)
         contact = Connectwise.submit_request('company/contacts', child_conditions=child_conditions)[0]
-        return cls(email, **contact)
+        return cls(**contact)
 
     @classmethod
     def fetch_all_internal(cls):
         conditions = 'company/id={}'.format(constants.CW_INTERNAL_COMPANY_ID)
         contacts = Connectwise.submit_request('company/contacts', conditions)
-        return [
-            cls(
-                [item['value'] for item in contact['communicationItems'] if item['communicationType'] == "Email"][0],
-                **contact
-            ) for contact in contacts]
+        return [cls(**contact) for contact in contacts]
 
     @classmethod
     def fetch_by_company_id(cls, company_id, additional_conditions=None):
@@ -35,11 +28,7 @@ class Contact:
         if additional_conditions:
             conditions += ' and {}'.format(additional_conditions)
         contacts = Connectwise.submit_request('company/contacts', conditions)
-        return [
-            cls(
-                [item['value'] for item in contact['communicationItems'] if item['communicationType'] == "Email"][0],
-                **contact
-            ) for contact in contacts]
+        return [cls(**contact) for contact in contacts]
 
     def get_phone(self, i=0):
         return [communicationItem['value']
