c83268a43c84ecc11c3891675c0c12e251edfbd5
Nick Alexander <nick@appkabob.com>
2017-06-15 15:30:40 -0500

    Expense entries can now be fetched by member_identifier. Added a method to get the ticket associated with the expense entry. Also added a get_charge_to_info method that will return a detailed string regardless of whether the expense entry is associated with a Ticket, an Activity, or a Charge Code

--- connectwise/expense.py
+++ connectwise/expense.py
@@ -1,4 +1,7 @@
 from decimal import Decimal
+
+from lib.connectwise_py.connectwise.activity import Activity
+from lib.connectwise_py.connectwise.ticket import Ticket
 from .connectwise import Connectwise
 
 
@@ -50,3 +53,49 @@ class ExpenseEntry:
 
         return [cls(**expense_entry) for expense_entry in
                                      Connectwise.submit_request('expense/entries', conditions)]
+
+    @classmethod
+    def fetch_by_member_identifier(cls, member_identifier, on_or_after=None, before=None):
+        conditions = ['member/identifier="{}"'.format(member_identifier)]
+        if on_or_after:
+            conditions.append('date>=[{}]'.format(on_or_after))
+        if before:
+            conditions.append('date<[{}]'.format(before))
+
+        return [cls(**expense_entry) for expense_entry in
+                Connectwise.submit_request('expense/entries', conditions)]
+
+    def get_ticket(self, tickets=[]):
+        if self.chargeToType == 'Activity':
+            return Activity.fetch_by_id(self.chargeToId)
+        elif self.chargeToType == 'ProjectTicket' or self.chargeToType == 'ServiceTicket':
+            if tickets:
+                return [ticket for ticket in tickets if self.chargeToId == ticket.id][0]
+            return Ticket.fetch_by_id(self.chargeToId)
+        return '{} {}'.format(self.chargeToType, self.chargeToId)
+
+    def get_charge_to_info(self, tickets=[], activities=[], charge_codes=[]):
+        if self.chargeToType == 'Activity':
+            if activities:
+                self.activity = [activity for activity in activities if self.chargeToId == activity][0]
+            else:
+                self.activity = Activity.fetch_by_id(self.chargeToId)
+            output = self.company['name']
+            output += ' / {}'.format(self.activity.opportunity['name'])
+            output += ' / Activity #{}: {}'.format(self.activity.id, self.activity['name'])
+
+        elif self.chargeToType == 'ProjectTicket' or self.chargeToType == 'ServiceTicket':
+            if tickets:
+                self.ticket = [ticket for ticket in tickets if self.chargeToId == ticket.id][0]
+            else:
+                self.ticket = Ticket.fetch_by_id(self.chargeToId)
+            output = self.company['name']
+            output += ' / {}'.format(self.ticket.project['name']) if self.ticket.project else ''
+            output += ' / {}'.format(self.ticket.phase['name']) if self.ticket.phase else ''
+            output += ' / Ticket #{}: {}'.format(self.ticket.id, self.ticket.summary)
+        else:
+            output = '{} {}'.format(self.chargeToType, self.chargeToId)
+
+        return output
+
+
