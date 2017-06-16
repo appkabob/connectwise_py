156ecfe47d277d54313ca87efd10c941527ad93f
Nick Alexander <nick@appkabob.com>
2017-06-16 13:32:20 -0500

    Can now fetch projects by id range, and also more easily get the business unit name based on the business unit id

--- connectwise/project.py
+++ connectwise/project.py
@@ -1,5 +1,6 @@
 import math
 
+import constants
 from lib.connectwise_py.connectwise.ticket import Ticket
 from .connectwise import Connectwise
 
@@ -27,6 +28,13 @@ class Project:
         conditions = 'id={}'.format(_id)
         return [cls(**project) for project in Connectwise.submit_request('project/projects', conditions)][0]
 
+    @classmethod
+    def fetch_by_id_range(cls, low_id=None, high_id=None):
+        conditions = []
+        if low_id: conditions.append('id>={}'.format(low_id))
+        if high_id: conditions.append('id<={}'.format(high_id))
+        return [cls(**project) for project in Connectwise.submit_request('project/projects', conditions)]
+
     def budget_days(self):
         return round(self.budgetHours / 8, 2)
 
@@ -34,6 +42,12 @@ class Project:
         if not tickets:
             tickets = Ticket.fetch_by_project_id(self.id)
         return round(sum([math.ceil(ticket.budgetHours / 8) * ticket.est_nbr_consultants() if ticket.budgetHours else 0 for ticket in tickets if ticket.serviceLocation['id'] == 1]), 2)
+
+    def business_unit_name(self):
+        business_unit_names = list(constants.BUSINESS_UNITS.keys())
+        business_unit_values = list(constants.BUSINESS_UNITS.values())
+        business_unit_index = business_unit_values.index(self.businessUnitId)
+        return business_unit_names[business_unit_index]
 
 
 class Phase:
