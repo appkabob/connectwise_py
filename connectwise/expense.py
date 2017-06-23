fdc68fed4e09b0e558f4fd7276ab796d5d61b20a
Nick Alexander <nick@appkabob.com>
2017-06-22 22:53:35 -0500

    get_charge_to_info can now return either a list or a string, to make it easier to style specific sections of it after you've made the call

--- connectwise/expense.py
+++ connectwise/expense.py
@@ -74,27 +74,32 @@ class ExpenseEntry:
             return Ticket.fetch_by_id(self.chargeToId)
         return '{} {}'.format(self.chargeToType, self.chargeToId)
 
-    def get_charge_to_info(self, tickets=[], activities=[], charge_codes=[]):
+    def get_charge_to_info(self, tickets=[], activities=[], charge_codes=[], return_type='string'):
         if self.chargeToType == 'Activity':
             if activities:
                 self.activity = [activity for activity in activities if self.chargeToId == activity][0]
             else:
                 self.activity = Activity.fetch_by_id(self.chargeToId)
-            output = self.company['name']
-            output += ' / {}'.format(self.activity.opportunity['name'])
-            output += ' / Activity #{}: {}'.format(self.activity.id, self.activity['name'])
+            output = [self.company['name']]
+            output.append('{}'.format(self.activity.opportunity['name']))
+            output.append('Activity #{}: {}'.format(self.activity.id, self.activity['name']))
 
         elif self.chargeToType == 'ProjectTicket' or self.chargeToType == 'ServiceTicket':
             if tickets:
                 self.ticket = [ticket for ticket in tickets if self.chargeToId == ticket.id][0]
             else:
                 self.ticket = Ticket.fetch_by_id(self.chargeToId)
-            output = self.company['name']
-            output += ' / {}'.format(self.ticket.project['name']) if self.ticket.project else ''
-            output += ' / {}'.format(self.ticket.phase['name']) if self.ticket.phase else ''
-            output += ' / Ticket #{}: {}'.format(self.ticket.id, self.ticket.summary)
+            output = [self.company['name']]
+            if self.ticket.project: output.append('{}'.format(self.ticket.project['name']))
+            if self.ticket.phase: output.append('{}'.format(self.ticket.phase['name']))
+            output.append('Ticket #{}: {}'.format(self.ticket.id, self.ticket.summary))
         else:
-            output = '{} {}'.format(self.chargeToType, self.chargeToId)
+            output = [self.company['name'], self.chargeToType, '{}'.format(self.chargeToId)]
+
+        if return_type == 'string':
+            return ' / '.join(output)
+        elif return_type == 'list':
+            return output
 
         return output
 
