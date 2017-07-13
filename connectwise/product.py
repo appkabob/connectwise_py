deafa08b643e1a849b12283fa0a3e0f1bb8266da
Nick Alexander <nick@appkabob.com>
2017-07-13 16:47:11 -0500

    Added billable_amount method that takes into account whether the product is billable, and also a total_cost method to premultiply the quantity * the unit cost

--- connectwise/product.py
+++ connectwise/product.py
@@ -4,6 +4,8 @@
 # from lib.connectwise_py.connectwise.ticket import Ticket
 # from lib.connectwise_py.connectwise.time_entry import TimeEntry
 # from .system_report import SystemReport
+from decimal import Decimal
+
 from .connectwise import Connectwise
 
 
@@ -49,9 +51,13 @@ class Product:
             'chargeToType="Project"'
         ]
         products = [cls(**product) for product in Connectwise.submit_request('procurement/products', conditions)]
-        if ticket_ids:
-            products.extend(cls.fetch_by_ticket_ids(ticket_ids, company_id, updated_on_or_after))
-        return [p for p in products if p.chargeToType == 'Project' or p.chargeToType == 'Ticket']
+        ticket_products = [p for p in products if p.chargeToType == 'Ticket']
+        ticket_product_ids = [p.id for p in ticket_products]
+        ticket_products.extend([p for p in products if p.chargeToType == 'Project' and p.id not in ticket_product_ids])
+        # if ticket_ids:
+        #     ticket_products = cls.fetch_by_ticket_ids(ticket_ids, company_id, updated_on_or_after)
+        # return [p for p in products if p.chargeToType == 'Project' or p.chargeToType == 'Ticket']
+        return ticket_products
 
     @classmethod
     def fetch_by_ticket_ids(cls, ticket_ids, company_id=None, updated_on_or_after=None):
@@ -62,6 +68,14 @@ class Product:
             conditions.append('lastUpdated>=[{}]'.format(updated_on_or_after))
         products = [cls(**product) for product in Connectwise.submit_request('procurement/products', conditions)]
         return [p for p in products if p.chargeToId in ticket_ids]
+
+    def billable_amount(self):
+        if self.billableOption == 'Billable':
+            return Decimal(round(self.quantity * self.price, 2))
+        return Decimal(0)
+
+    def total_cost(self):
+        return Decimal(round(self.quantity * self.cost, 2))
 
 
 class CatalogProduct:
