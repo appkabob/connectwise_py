55bb75b3278daf163b00744c7ae84468903577e0
Nick Alexander <nick@appkabob.com>
2017-11-03 01:11:52 -0500

    Added to_dict method, and a service_location method to infer On-Site/Off-Site status from Schedule Entries and/or Tickets for vendor services

--- connectwise/product.py
+++ connectwise/product.py
@@ -5,12 +5,14 @@
 # from lib.connectwise_py.connectwise.time_entry import TimeEntry
 # from .system_report import SystemReport
 from decimal import Decimal
+from pprint import pprint
 
 from .connectwise import Connectwise
 
 
 class Product:
     def __init__(self, **kwargs):
+        self.purchaseDate = None
         for kwarg in kwargs:
             setattr(self, kwarg, kwargs[kwarg])
 
@@ -31,6 +33,25 @@ class Product:
         if internal_notes: conditions['internalNotes'] = internal_notes
         product = Connectwise.submit_request('procurement/products', conditions, verb='POST')
         return cls(**product)
+
+    def to_dict(self, include_self=False, schedule_entries=[], tickets=[], activities=[]):
+        """
+        Get a representation of the Product object as a Python Dictionary,
+        including calculated values from methods.
+
+        If you can provide a list of schedule entries, tickets, or activities that
+        contain records potentially relating to this Product, you can get a more
+        accurate Service Location for relevant Products.
+
+        :param include_self: when True, include the original object under the dictionary
+        key 'self' so that you have it in case you need to reference it later
+        """
+        dict = {}
+        dict['billable_amount'] = self.billable_amount()
+        dict['total_cost'] = self.total_cost()
+        dict['service_location'] = self.service_location(schedule_entries, tickets, activities)
+        if include_self: dict['self'] = self
+        return {**vars(self), **dict}
 
     @classmethod
     def fetch_all(cls):
@@ -80,6 +101,7 @@ class Product:
             'chargeToType="Project"'
         ]
         products = [cls(**product) for product in Connectwise.submit_request('procurement/products', conditions)]
+        pprint([vars(p) for p in products])
         ticket_products = [p for p in products if p.chargeToType == 'Ticket']
         ticket_product_ids = [p.id for p in ticket_products]
         ticket_products.extend([p for p in products if p.chargeToType == 'Project' and p.id not in ticket_product_ids])
@@ -105,6 +127,20 @@ class Product:
 
     def total_cost(self):
         return Decimal(round(self.quantity * self.cost, 2))
+
+    def service_location(self, schedule_entries=[], tickets=[], activities=[]):
+        schedule_entry = [s.where['name'] for s in schedule_entries
+                          if s.dateStart[:10] == self.purchaseDate
+                          and s.objectId == self.chargeToId]
+        if schedule_entry: return schedule_entry[0]
+
+        ticket = [t.serviceLocation['name'] for t in tickets if t.id == self.chargeToId]
+        if ticket: return ticket[0]
+
+        activity = [a.where for a in activities if a.id == self.chargeToId]
+        if activity: return activity[0]
+
+        return 'On-Site'
 
 
 class CatalogProduct:
