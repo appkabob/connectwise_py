9a958aa6c9a9dec03463fe922ea1bbebec42db0f
Nick Alexander <nick@appkabob.com>
2017-12-12 16:00:18 -0600

    Made fetching future and past holidays generalizable for any given date

--- connectwise/holiday.py
+++ connectwise/holiday.py
@@ -23,15 +23,28 @@ class Holiday:
         return [cls(**holiday) for holiday in SystemReport.fetch_holiday_list()]
 
     @classmethod
-    def fetch_future(cls):
-        return [d for d in cls.fetch_all() if d.Holiday_Date > datetime.datetime.now().strftime('%Y-%m-%d')]
+    def fetch_passed(cls, before=None):
+        if not before: before = datetime.datetime.now().strftime('%Y-%m-%d')
+        return [h for h in cls.fetch_all() if h.Holiday_Date < before]
 
     @classmethod
-    def fetch_remaining_this_fy(cls):
-        on_or_after, before = Connectwise.current_fy()
-        return [d for d in cls.fetch_future() if d.Holiday_Date < before]
+    def fetch_passed_this_fy(cls, before=None):
+        if not before: before = datetime.datetime.now().strftime('%Y-%m-%d')
+        date_start_this_fy, date_start_next_fy = Connectwise.fy_of_date(before)
+        return [h for h in cls.fetch_passed(before) if h.Holiday_Date >= date_start_this_fy]
 
     @classmethod
-    def fetch_this_fy(cls):
-        on_or_after, before = Connectwise.current_fy()
-        return [d for d in cls.fetch_all() if on_or_after <= d.Holiday_Date < before]
+    def fetch_future(cls, on_or_after=None):
+        if not on_or_after: on_or_after = datetime.datetime.now().strftime('%Y-%m-%d')
+        return [h for h in cls.fetch_all() if h.Holiday_Date >= on_or_after]
+
+    @classmethod
+    def fetch_remaining_this_fy(cls, on_or_after=None):
+        if not on_or_after: on_or_after = datetime.datetime.now().strftime('%Y-%m-%d')
+        date_start_this_fy, date_start_next_fy = Connectwise.fy_of_date(on_or_after)
+        return [h for h in cls.fetch_future(on_or_after) if h.Holiday_Date < date_start_next_fy]
+
+    @classmethod
+    def fetch_this_fy(cls, of_date=None):
+        on_or_after, before = Connectwise.fy_of_date(of_date)
+        return [h for h in cls.fetch_all() if on_or_after <= h.Holiday_Date < before]
