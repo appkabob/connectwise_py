30cdfe221b0c539475f437e677dd6c4c0f904672
Nick Alexander <nick@appkabob.com>
2017-12-13 21:55:52 -0600

    Can now fetch members from CW by type. Can also fetch Contact corresponding to Member for internal company. Also can fetch vacation agreements for Member

--- connectwise/member.py
+++ connectwise/member.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 import constants
+from lib.connectwise_py.connectwise.agreement import Agreement
+from lib.connectwise_py.connectwise.contact import Contact
 from .connectwise import Connectwise
 
 
@@ -28,7 +30,18 @@ class Member:
 
     @classmethod
     def fetch_all_members(cls):
-        conditions = ['identifier!="APIMember"']
+        conditions = ['identifier!="APIMember" and identifier!="screenconnect" and identifier!="quosal"']
+        filters = {'orderBy': 'lastName asc'}
+        return [cls(**member) for member in Connectwise.submit_request('system/members', conditions, filters)]
+
+    @classmethod
+    def fetch_by_type_name(cls, type_name):
+        """
+        Return members filtered by type. For example, "Salaried Employee"
+        :param type_name: str: member type, e.g. "Salaried Employee"
+        :return: list of Members
+        """
+        conditions = 'type/name="{}"'.format(type_name)
         filters = {'orderBy': 'lastName asc'}
         return [cls(**member) for member in Connectwise.submit_request('system/members', conditions, filters)]
 
@@ -44,3 +57,22 @@ class Member:
 
     def daily_cost(self, on_date='today'):
         return round(self.hourly_cost(on_date) * 8, 2)
+
+    def fetch_internal_contact(self):
+        return Contact.fetch_by_email(self.officeEmail)
+
+    def fetch_vacation_agreements(self, agreements=[], contacts=[]):
+        contact = None
+        if len(contacts) > 0:
+            contact = [c for c in contacts if self.officeEmail == c.get_email()]
+            if len(contact) > 0: contact = contact[0]
+        else:
+            contact = self.fetch_internal_contact()
+        if len(agreements) == 0:
+            agreements = Agreement.fetch_vacation_agreements()
+        agreements = [a for a in agreements if contact and contact.id == a.contact['id']]
+        # print(self.identifier, contact, agreements)
+        if contact:
+            return [a for a in agreements if a.contact['id'] == contact.id]
+        else:
+            return []
